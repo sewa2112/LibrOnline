9156fb76b42387a0e8ea4454e3d75692613cfa54
Valentina-coder <va.cortezz@duocuc.cl>
2026-05-07 15:22:08 -0400

    creacion de microservicio Asistencia Básico

---
+++ src/main/java/com/example/Asistencia/Models/Request/CrearAsistenciaRequest.java
@@ -0,0 +1,19 @@
+package com.example.Asistencia.Models.Request;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CrearAsistenciaRequest {
+
+    @NotBlank
+    private String estudiante;
+    @NotBlank
+    private LocalDate fecha;
+    @NotBlank   
+    private String estado;
+
+  
+}
