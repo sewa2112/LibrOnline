61bbb5f5fd5d66895628bdf175ae59860b07b48f
Sebastian <se.osoriob@duocuc.cl>
2026-05-14 17:13:01 -0400

    Gestion de Reuniones terminado con swagger

---
+++ Gestion_Reuniones/src/main/java/com/example/Gestion_Reuniones/config/OpenApiConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.Gestion_Reuniones.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestión de Reuniones API")
+                        .version("1.0")
+                        .description("API para gestionar reuniones, incluyendo creación, actualización, eliminación y consulta de reuniones."));
+    }
+}
