f32af2d2cabe13bfe36aadd6ffa257957da5c86a
panda2048-max <fabr.morales@duocuc.cl>
2026-05-16 20:57:41 -0400

    conexion entre microservicios

--- matriculas/src/main/java/com/example/matriculas/models/request/Agregar_hoja_vida.java
+++ matriculas/src/main/java/com/example/matriculas/models/request/Agregar_hoja_vida.java
@@ -2,6 +2,7 @@ package com.example.matriculas.models.request;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -10,5 +11,8 @@ public class Agregar_hoja_vida {
     
     @NotNull
     private LocalDate fecha_cracion;
+
+    @NotBlank
+    private int id_usuarios;
     
 }
